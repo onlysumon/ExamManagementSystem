62df449a5e9feb6ad0285d69379ee91d72c0e557
Sharif <onlysumon@yahoo.com>
2014-02-05 18:14:16 -0600

    All Files Included

---
+++ OnlineExamSystem/OnlineExamSystem/Models/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineExamSystem.Models
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string Name { get; set; }
+        public string? ImageUrl { get; set; }
+        public bool Type { get; set; }
+        public int Marks { get; set; }
+        public int Duration { get; set; }
+        public int CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual IList<Option> Options { get; set; }
+    }
+}
---
+++ OnlineExamSystem/OnlineExamSystem/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineExamSystem.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public Gender? Gender { get; set; }
+        public string Email { get; set; }    
+        public string InstituteName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string LoginName { get; set; }
+        public string LoginPassword { get; set; }
+        public string? PhotoUrl { get; set; }
+
+        public virtual IList<Course> Courses { get; set; }
+    }
+
+    public enum Gender
+    {
+        M, F
+    }
+}
